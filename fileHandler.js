c39a888f4c7f65ea1f1c7fcf6f145cc499262842
Poorna-Athreya <Poorna_athreya@mckinsey.com>
2022-02-21 22:48:26 +0530

    Feat: Reading file contet into an object (async)

--- fileHandler.js
+++ fileHandler.js
@@ -12,16 +12,17 @@ const readAndFilterFile = (filename, filterValue = null) => new Promise((resolve
   if (!filename || typeof filename !== 'string' || filename.slice(-4) !== '.txt') reject(new Error('Invalid, enter a proper filepath!'));
   try {
     fs.readFile(filename, (err, data) => {
-      if (err) return reject(err);
+      if (err) return reject(new Error('File cannot be found!'));
       if (filterValue) return resolve((data.toString().split('\r\n')).filter((item) => item.toLowerCase().charAt(0) === filterValue.toLowerCase()));
       return resolve(data.toString().split('\r\n'));
     });
   } catch (error) { console.log(error.message); }
 });
 const getFilesInDirectory = (dirname = 'seed') => new Promise((resolve, reject) => {
+  if (!dirname || typeof dirname !== 'string') throw new Error('Invalid, Cannot Find Directory!');
   fs.readdir(dirname, async (err, files) => {
     const allFiles = [];
-    if (err) return reject(new Error('Cannot Read Directory!'));
+    if (err) return reject(new Error('Invalid, Cannot Read Directory!'));
     try {
       files.forEach((file) => {
         if (file.slice(-4) === '.txt') {
@@ -33,6 +34,8 @@ const getFilesInDirectory = (dirname = 'seed') => new Promise((resolve, reject) 
   });
 });
 const putFilesIntoObject = async (dirname = 'seed', filterValue = null) => {
+  if (!dirname || typeof dirname !== 'string') throw new Error('Invalid, Cannot Find Directory!');
+  if (filterValue && typeof filterValue !== 'string') throw new Error('Invalid, Enter proper filter character(s)!');
   const fileNames = await getFilesInDirectory(dirname);
   const fileContent = {};
   await Promise.all(fileNames.map(async (file) => {
@@ -40,11 +43,16 @@ const putFilesIntoObject = async (dirname = 'seed', filterValue = null) => {
     const key = file.slice(0, -4);
     fileContent[key] = results;
   }));
+  return fileContent;
 };
-putFilesIntoObject();
+// putFilesIntoObject();
 // async function main() {
 //   const fruits = await readAndFilterFile('files\\fruits.txt');
 //   console.log('Fruits: ', fruits);
+//   const files = await getFilesInDirectory();
+//   console.log(files);
+//   const fileObj = await putFilesIntoObject();
+//   console.log(fileObj);
 // }
 // main();
 module.exports = {
--- tests/fileHandler.test.js
+++ tests/fileHandler.test.js
@@ -1,7 +1,8 @@
+/* eslint-disable no-unused-vars */
 // File Handling
 const fs = require('fs');
 // const { isTypedArray } = require('util/types');
-const { readAndFilterFile } = require('../fileHandler');
+const { readAndFilterFile, getFilesInDirectory, putFilesIntoObject } = require('../fileHandler');
 
 describe('ReadAndFilterFile function', () => {
   it('should read and display the data in fruits.txt in form of an array', async () => {
@@ -40,6 +41,81 @@ describe('ReadAndFilterFile function', () => {
       const data = await readAndFilterFile(5);
     } catch (err) {
       expect(err.message).toBe('Invalid, enter a proper filepath!');
+    }
+  });
+});
+describe('GetFilesInDirectory function', () => {
+  it('should get file names of files in the directory in an array', async () => {
+    jest.spyOn(fs, 'readdir').mockImplementation((filepath, callback) => {
+      callback(null, ['beverages.txt', 'fruits.txt', 'vegetables.txt']);
+    });
+    const files = await getFilesInDirectory('seed\\fruits.txt');
+    expect(files).toEqual(['beverages.txt', 'fruits.txt', 'vegetables.txt']);
+  });
+  it('should return invalid message if directory is not found', async () => {
+    jest.spyOn(fs, 'readdir').mockImplementation((filepath, callback) => {
+      callback(new Error('Invalid, Cannot Read Directory!'), null);
+    });
+    try {
+      const data = await getFilesInDirectory('seeds');
+    } catch (err) {
+      expect(err.message).toBe('Invalid, Cannot Read Directory!');
+    }
+  });
+  it('should return invalid message when directory name is not a string', async () => {
+    try {
+      const data = await getFilesInDirectory(5);
+    } catch (err) {
+      expect(err.message).toBe('Invalid, Cannot Find Directory!');
+    }
+    // expect(await getFilesInDirectory(5)).toThrow('Invalid, Cannot Find Directory!');
+  });
+});
+
+describe('PutFilesIntoObject function', () => {
+//   it('should return an object with file names as key with array of content as value',
+// async () => {
+//     jest.spyOn(fs, 'readdir').mockImplementation((filepath, callback) => {
+//       callback(null, {
+//         fruits: ['mango', 'banana', 'orange', 'apple'],
+//         vegetables: [
+//           'carrot',
+//           'beans',
+//           'potato',
+//           'spinach',
+//           'brocolli',
+//           'capsicum',
+//           'beetroot',
+//         ],
+//         beverages: [''],
+//       });
+//     });
+//     expect(await putFilesIntoObject('seed')).toEqual({
+//       fruits: ['mango', 'banana', 'orange', 'apple'],
+//       vegetables: [
+//         'carrot',
+//         'beans',
+//         'potato',
+//         'spinach',
+//         'brocolli',
+//         'capsicum',
+//         'beetroot',
+//       ],
+//       beverages: [''],
+//     });
+//   });
+  it('should return invalid message when directory name is not string', async () => {
+    try {
+      const data = await putFilesIntoObject(5);
+    } catch (err) {
+      expect(err.message).toBe('Invalid, Cannot Find Directory!');
+    }
+  });
+  it('should return invalid message if filterValue is not null and not a character', async () => {
+    try {
+      const data = await putFilesIntoObject('seeds', 5);
+    } catch (err) {
+      expect(err.message).toBe('Invalid, Enter proper filter character(s)!');
     }
   });
 });
