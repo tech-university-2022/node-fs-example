08bf8cfd326ee36227f1147bf93f14d026887029
Poorna-Athreya <Poorna_athreya@mckinsey.com>
2022-02-23 13:11:19 +0530

    Refactor: Mocking util functions in testing

--- src/fileHandler.js
+++ src/fileHandler.js
@@ -1,12 +1,12 @@
 const path = require('path');
-const { promisifyReadFile, promisifyReadDir, removeFromFile } = require('./utils/fileUtilities');
+const utils = require('./utils/fileUtilities');
 
 const readAndFilterFiles = async (directoryPath, filterCharacter = null) => {
   if (typeof directoryPath !== 'string') throw Error('Invalid Directory Path!');
   if (filterCharacter && typeof filterCharacter !== 'string') throw Error('Invalid Filter Character!');
-  const rawFileNames = await promisifyReadDir(directoryPath);
+  const rawFileNames = await utils.promisifyReadDir(directoryPath);
   const fileNames = rawFileNames.map((fileName) => path.parse(fileName).name);
-  const allFilePromises = rawFileNames.map((file) => promisifyReadFile(`${directoryPath}/${file}`, filterCharacter));
+  const allFilePromises = rawFileNames.map((file) => utils.promisifyReadFile(`${directoryPath}/${file}`, filterCharacter));
   let allFilesContent = await Promise.all(allFilePromises);
   allFilesContent = allFilesContent.reduce((allContent, perFileContent, index) => ({
     ...allContent,
@@ -20,5 +20,6 @@ module.exports = {
   readAndFilterFiles,
 };
 // (async () => {
-//   console.log(await removeFromFile('./seed/vegetables.txt', 'C'));
+//   const veggiesWithC = await readAndFilterFiles('./seed', 'C');
+//   console.log(veggiesWithC);
 // })();
--- tests/fileHandler.test.js
+++ tests/fileHandler.test.js
@@ -1,7 +1,16 @@
 const { readAndFilterFiles } = require('../src/fileHandler');
+const utils = require('../src/utils/fileUtilities');
 
 describe('ReadAndFilterfile function', () => {
+  jest.spyOn(utils, 'promisifyReadDir').mockResolvedValue(['beverages.txt', 'fruits.txt', 'vegetables.txt']);
   it('should return an object with file name as key and array of content', async () => {
+    jest.spyOn(utils, 'promisifyReadFile').mockResolvedValueOnce(['']).mockResolvedValueOnce(['mango', 'banana', 'orange', 'apple']).mockResolvedValueOnce(['carrot',
+      'beans',
+      'potato',
+      'spinach',
+      'brocolli',
+      'capsicum',
+      'beetroot']);
     const readAndFilterSeed = await readAndFilterFiles('./seed');
     expect(readAndFilterSeed).toEqual({
       beverages: [''],
@@ -18,6 +27,7 @@ describe('ReadAndFilterfile function', () => {
     });
   });
   it('should return object with filtered content if Filter Character is given', async () => {
+    jest.spyOn(utils, 'promisifyReadFile').mockResolvedValueOnce([]).mockResolvedValueOnce([]).mockResolvedValueOnce(['carrot', 'capsicum']);
     const readAndFilterSeedC = await readAndFilterFiles('./seed', 'C');
     expect(readAndFilterSeedC).toEqual({ beverages: [], fruits: [], vegetables: ['carrot', 'capsicum'] });
   });
@@ -30,5 +40,11 @@ describe('ReadAndFilterfile function', () => {
     try {
       await readAndFilterFiles(5);
     } catch (error) { expect(error.message).toBe('Invalid Directory Path!'); }
+  });
+  it('should return invalid message when directory is invalid', async () => {
+    jest.spyOn(utils, 'promisifyReadDir').mockRejectedValue(Error('Directory not found!'));
+    try {
+      await readAndFilterFiles('fruits');
+    } catch (error) { expect(error.message).toBe('Directory not found!'); }
   });
 });
