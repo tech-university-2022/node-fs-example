f25d3f5721f2535f9001e81e60758c29abb948eb
Kiranyadav1234 <Kiran_Yadav@mckinsey.com>
2022-02-24 01:17:43 +0530

    feat:add all the reusable function in operator.js

---
+++ src/utils/operator.js
@@ -0,0 +1,42 @@
+const fs = require('fs');
+
+const promisefyReaddir = (directoryName) => new Promise((resolve, reject) => {
+  fs.readdir(directoryName, 'UTF-8', (err, data) => {
+    if (err) { reject(new Error('file name are not accesible')); } else {
+      resolve(data);
+    }
+  });
+});
+const promisefyFileRead = (filePath) => new Promise((resolve, reject) => {
+  fs.readFile(filePath, 'UTF-8', (err, data) => {
+    if (err) {
+      reject(err);
+    } else {
+      resolve(data.toString().split('\r\n').filter((currfiledata) => currfiledata !== ''));
+    }
+  });
+});
+const promisefyWriteFile = (filepath, dataToBeAdded) => new Promise((resolve, reject) => {
+  fs.writeFile(filepath, dataToBeAdded, (err) => {
+    if (err) {
+      reject(new Error('cannot open file'));
+    } else {
+      resolve('successfully data added');
+    }
+  });
+});
+
+const promisefyAppendFile = (filepath, dataToBeAppend) => new Promise((resolve, reject) => {
+  fs.appendFile(filepath, dataToBeAppend, 'UTF-8', (err) => {
+    if (err) {
+      reject(new Error('cannot append data '));
+    } else {
+      resolve('appended data succesfully');
+    }
+  });
+});
+
+module.exports = {
+  promisefyReaddir, promisefyFileRead, promisefyWriteFile, promisefyAppendFile,
+};
+// ctrl+b:-to increase the size
