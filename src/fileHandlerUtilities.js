a9505857fc05bb532d294c4ae37db59c1e025071
lavanyaam1 <LAVANYA_A_M@mckinsey.com>
2022-02-24 13:02:27 +0530

    refactor:modified fileHandlerUtilities

--- src/fileHandlerUtilities.js
+++ src/fileHandlerUtilities.js
@@ -1,8 +1,8 @@
-const { resolve } = require('dns');
 const fs = require('fs');
 const path = require('path');
 
 const promisfyReadDir = (directoryPath) => {
+  // if(typeof(directoryPath)!='string')
     return new Promise((resolve,reject) => {
         fs.readdir(directoryPath, 'utf-8' , (error,data) => {
             if(error) {
@@ -68,7 +68,6 @@ const readAndFilterFiles = async (filePath, filterCharacter = null) => {
     })
 })
 }
-
 
 module.exports={
   promisfyReadDir,
