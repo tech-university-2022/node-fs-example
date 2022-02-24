b1ce4a589efe431d7a605c5747478e8664c03f5a
Twissa Modi <Twissa_Modi@mckinsey.com>
2022-02-24 11:59:26 +0530

    feat: add file handler

--- src/fileHandler2.js
+++ src/fileHandler2.js
@@ -1,48 +1,39 @@
-const fs=require('fs');
+
 const path=require('path');
-const {getFileNames}=require('../utils/fileNamesRetriever.js');
-const {getDataFromFile}=require('../utils/fileReader.js');
+const utils=require('../utils/fileManipulator.js');
+const readMultipleFiles=require('../utils/readMultipleFiles');
+const filterData=(dataInFiles,filterWithStartingLetter)=>{
+    if(dataInFiles.constructor !==Object){
+        throw new Error('You must enter an object.');
+    }
+    for(const key in dataInFiles){
+        dataInFiles[key]=dataInFiles[key].filter((currentData)=>currentData.startsWith(filterWithStartingLetter));
+    }
+    return dataInFiles;
+}
 
-const readAllFiles=async (directoryPath,filterWithStartingLetter='')=>{
-    return new Promise(async (resolve,reject)=>{
-        const fileNamesWithExtension=await getFileNames(directoryPath);
-        const fileNames=fileNamesWithExtension.map(file=>path.parse(file).name);
-        const promises=fileNamesWithExtension.map((file)=>getDataFromFile(`${directoryPath}\\${file}`));
-        console.log(fileNamesWithExtension);
-        const fileData=await Promise.all(promises);
-        const data=fileData.reduce((acc,currentFileContent,index)=>{
-            return{
-                ...acc,
-                [fileNames[index]]:currentFileContent,
-            }
-        },{});
-        if(filterWithStartingLetter!==''){
-            //resolve(filterData(data,filterWithStartingLetter));
-        }
-        resolve(data);
+const removeData= async(filePath,startingLetter)=>{
+    return new Promise(async(fulfill,reject)=>{
+        let data;
+        try{
+            data=await utils.getDataFromFile(filePath);
+        }catch(err){
+            return reject(err);
+        };
+            data=data.filter((element)=>element[0]!==startingLetter);
+            data=data.toString().replaceAll(',','\r\n');
+            await utils.writeToFile(filePath,data);
+            try{
+                const directoryPath=filePath.substring(0,filePath.lastIndexOf("\\")+1);
+                return fulfill(await readMultipleFiles.readAllFiles(directoryPath));
+            }catch(err){
+                reject(err);
+            };
     });
-};
-const writeToFile=(fileName,content)=>{
-    return new Promise((fulfill,reject)=>{
-        fs.writeFile(fileName,content,(err)=>{
-            if(err){
-                reject(new Error('Data could not be written'));
-            }
-            fulfill('data written successfully');
-        })
-    })
-}
-const appendToFile=(fileName,content)=>{
-    return new Promise((fulfill,reject)=>{
-        fs.appendFile(fileName,content,(err)=>{
-            if(err){
-                reject(new Error('Data could not be written'));
-            }
-            fulfill('data written successfully');
-        })
-    })
 }
 
-readAllFiles('C:\\Users\\Twissa Modi\\node-fs-example\\seed').then(value=>console.log(value),null);
-module.exports=readAllFiles;
+module.exports={
+    filterData,
+    removeData,
+};
 
